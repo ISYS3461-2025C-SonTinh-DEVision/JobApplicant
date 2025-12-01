a862142a642b5b8ff36f572350276f2c3a3dec9b
Minh Dinh <dinhxuanminh1234@gmail.com>
2025-12-02 00:38:26 +0700

    Backend initialized (refs #69)

---
+++ backend/src/main/java/com/DEVision/JobApplicant/JobApplicantApplication.java
@@ -0,0 +1,13 @@
+package com.DEVision.JobApplicant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicantApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/DEVision/JobApplicant/JobApplicantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DEVision.JobApplicant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
