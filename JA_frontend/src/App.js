e7ab2bae0482a1144956b16e1f2884a0694a8458
Aesitof <s3924763@rmit.edu.vn>
2025-12-05 17:59:26 +0700

    frontend init

---
+++ JA_frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import GridTable from './component/grid_table/GridTable';
+import GridTableCard from './component/grid_table/GridTableCard';
+import SearchBar from './component/search_bar/SearchBar';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      {/* <NewMetroLineForm /> */}
+
+      <GridTable/>
+
+
+    </div>
+  );
+}
+
+export default App;
+
